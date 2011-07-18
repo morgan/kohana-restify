1ac9639cc502cf40006e547275a7421aac462a18
Micheal Morgan <micheal@morgan.ly>
2011-07-18 19:16:59 -0400

    Segmented out header and footer

---
+++ views/restify/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	
+	<title>restify.io - For RESTful requests.</title>
+	
+	<?php echo HTML::style($path . 'css/reset.css'), PHP_EOL; ?>
+	<?php echo HTML::style($path . 'css/smoothness/jquery-ui-1.8.14.custom.css'), PHP_EOL; ?>
+	<?php echo HTML::style($path . 'css/prettify.css'), PHP_EOL; ?>
+	<?php echo HTML::style($path . 'css/styles.css'); ?>
+</head>
+
+<body>
--- views/restify/index.php
+++ views/restify/index.php
@@ -1,18 +1,5 @@
 <?php defined('SYSPATH') or die('No direct script access.') ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	
-	<title>restify.io - For RESTful requests.</title>
-	
-	<?php echo HTML::style($path . 'css/reset.css'), PHP_EOL; ?>
-	<?php echo HTML::style($path . 'css/smoothness/jquery-ui-1.8.14.custom.css'), PHP_EOL; ?>
-	<?php echo HTML::style($path . 'css/prettify.css'), PHP_EOL; ?>
-	<?php echo HTML::style($path . 'css/styles.css'); ?>
-</head>
-
-<body>
+<?php echo View::factory('restify/header', array('path' => $path)); ?>
 
 	<div id="wrapper">
 
@@ -131,5 +118,5 @@
 			}
 		};		
 	</script>
-</body>
-</html>
+	
+<?php echo View::factory('restify/footer'); ?>
